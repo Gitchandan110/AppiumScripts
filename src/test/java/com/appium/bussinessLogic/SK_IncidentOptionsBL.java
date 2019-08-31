381b9f74f7b61fa98f0ab6752503668e3f54a772
chandan kumar <chandan.kumar@netsutra.com>
2019-08-31 13:54:47 +0530

    c

--- src/test/java/com/appium/bussinessLogic/SK_IncidentOptionsBL.java
+++ src/test/java/com/appium/bussinessLogic/SK_IncidentOptionsBL.java
@@ -1,5 +1,25 @@
 package com.appium.bussinessLogic;
 
-public class SK_IncidentOptionsBL {
+import com.appium.pageobject.SK_IncidentOptionsPO;
+
+public class SK_IncidentOptionsBL extends SK_IncidentOptionsPO {
+	
+	
+	public void clickPhotoIncident() {
+		
+		try {
+			
+			if(btnPhotoAudioLocationIncident().isDisplayed()) {
+				
+				btnPhotoAudioLocationIncident().click();
+				System.out.println("Photo Incident selected");
+				
+			}
+		
+		} catch (Exception ex) {
+			
+			System.out.println("Exception in Phot Incident:" + ex.getMessage());
+		}
+	}
 
 }
